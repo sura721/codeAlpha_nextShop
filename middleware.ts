79daa00e5f363f65deff708b51c3f265b537690c
sura721 <surafeladmas721@gmail.com>
2026-07-19 00:05:26 +0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,27 @@
- 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
- const isPublicRoute = createRouteMatcher([
-   '/', 
+const isPublicRoute = createRouteMatcher([
+  '/', 
   '/product/(.*)', 
   '/products(.*)',  
   '/about', 
   '/contact',  
-
-   '/robots.txt',
+  '/robots.txt',
   '/sitemap.xml',
-  
-   '/sign-in(.*)',
+  '/sign-in(.*)',
   '/sign-up(.*)',
-
-   '/api/inngest',
+  '/api/inngest',
   '/api/uploadthing',
 ]);
 
-export default clerkMiddleware((auth, req) => {
-   if (!isPublicRoute(req)) {
-    auth.protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
 });
 
 export const config = {
-   matcher: [
+  matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
